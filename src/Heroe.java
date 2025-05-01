ea4c6ed2913ba48f2fcc936b82cc871e1636d545
borregota <ronnydavid9@hotmail.com>
2025-05-01 14:46:14 -0500

    EO

--- src/Heroe.java
+++ src/Heroe.java
@@ -1,2 +1,45 @@
 public class Heroe {
+    private String nombre;
+    private String raza;
+    private int nivelP;
+    private String habilidad;
+
+    public Heroe(String nombre, String raza, int nivelP, String habilidad) {
+        this.nombre = nombre;
+        this.raza = raza;
+        this.nivelP = nivelP;
+        this.habilidad = habilidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public int getNivelP() {
+        return nivelP;
+    }
+
+    public void setNivelP(int nivelP) {
+        this.nivelP = nivelP;
+    }
+
+    public String getHabilidad() {
+        return habilidad;
+    }
+
+    public void setHabilidad(String habilidad) {
+        this.habilidad = habilidad;
+    }
 }
